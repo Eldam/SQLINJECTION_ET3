fc2ed6af8df38b6e4dfe3fdaab8d91135aec3ee1
osgonzalez <osgonzalez@esei.uvigo.es>
2017-12-18 20:24:05 +0100

    Modify AssinToGroup_Commit

--- Controllers/User_ASSINGTOGROUP_Controller.php
+++ Controllers/User_ASSINGTOGROUP_Controller.php
@@ -24,36 +24,50 @@ else {
         new MESSAGE("No posee permisos para la accion solicitada", '../index.php');
     } else {
 
+        //Si la variable getGroups esta asignada
+        //se obtiene todos los grupos que a los que no pertence el usuario
+        //y se muestran en la vista ASSINGTOGROUP
+        //de lo contrario asigna el usuario a los grupos solicitados
+        if(!isset($_REQUEST["getGroups"])) {
 
+            //Se crea un DAO con la PK
+            //y se obtiene la lista de grupos a los que pertenece
+            include_once '../Models/User_Model.php';
+            $userDAO = new UserDAO($_REQUEST['login'], "");
+            $userIdGroups = $userDAO->getGroups();
 
-        //Se crea un DAO con la PK
-        //y se obtiene la lista de grupos a los que pertenece
-        include_once '../Models/User_Model.php';
-        $userDAO = new UserDAO($_REQUEST['login'], "");
-        $userIdGroups = $userDAO->getGroups();
+            //echo "<br><h1 style='color: red'> "."".implode($userIdGroups)."</h1><br>";
 
-        //echo "<br><h1 style='color: red'> "."".implode($userIdGroups)."</h1><br>";
+            //Se crea un DAO con la PK
+            //y se obtiene la lista de todos grupos
+            include_once '../Models/Group_Model.php';
+            $allGroups = (new GroupDAO(""))->SEARCH();
 
-        //Se crea un DAO con la PK
-        //y se obtiene la lista de todos grupos
-        include_once '../Models/Group_Model.php';
-        $allGroups= (new GroupDAO(""))->SEARCH();
+            //Se crea un array vacio
+            $notUserGroups = Array();
 
-        //Se crea un array vacio
-        $notUserGroups = Array();
-
-        //Se añaden al array $notUserGroups todos los grupos que no posee el usuario
-        while ($group = mysqli_fetch_array($allGroups)){
-            if(!in_array($group["IdGrupo"],$userIdGroups)){
-                $notUserGroups[] = $group;
+            //Se añaden al array $notUserGroups todos los grupos que no posee el usuario
+            while ($group = mysqli_fetch_array($allGroups)) {
+                if (!in_array($group["IdGrupo"], $userIdGroups)) {
+                    $notUserGroups[] = $group;
+                }
             }
+
+
+            //se muestra la vista ASSINGTOGROUP con todos los parametros obtenidos
+            include_once '../Views/User_ASSINGTOGROUP_View.php';
+            new User_ASSINGTOGROUP_View($notUserGroups);
+
+
+
+        }else{
+            //Se crea un DAO con la PK
+            include_once '../Models/User_Model.php';
+            $userDAO = new UserDAO($_REQUEST['login'], "");
+
+            
+
         }
-
-
-
-        //se muestra la vista ASSINGTOGROUP con todos los parametros obtenidos
-        include_once '../Views/User_ASSINGTOGROUP_View.php';
-        new User_ASSINGTOGROUP_View($notUserGroups);
 
     }
 
