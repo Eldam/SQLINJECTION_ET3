159fcfea614a4a501846d9ba639fa8255ab364c1
eldam <mbmartin@esei.uvigo.es>
2017-12-23 18:22:50 +0100

    Add ImportationUSer edit view and controller

--- Views/ImportationUser_EDIT_View.php
+++ Views/ImportationUser_EDIT_View.php
@@ -8,5 +8,81 @@
 
 class ImportationUser_EDIT_View
 {
+    var $response;
 
+    function __construct($response){
+        $this->response = $response;
+        $this->render();
+    }
+
+    function render(){
+        include '../Locales/Header.html';
+        include '../Locales/LateralBar.php';
+        $importation = mysqli_fetch_array($this->response);
+        ?>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../Locales/ImportationUser_EDIT.css">
+
+
+        <div class="EditFormContainer">
+
+            <form action="../Controllers/Work_EDIT_Controller.php" onsubmit="comprobarFormsVacios(this)" method="post">
+
+                <div class="EditFormHeader">
+                    <h1>Editar Entrega</h1>
+                </div>
+
+                <div class="EditForm1">
+                    <br>
+                    <div class="group">
+                        <label for="login" class="label">Login*:</label>
+                        <input id="login" name="login"  type="text" class="input" value="<?php echo $importation['login'];?>" readonly>
+                    </div>
+                    <div class="group">
+                        <label for="IdTrabajo" class="label">Id Trabajo*:</label>
+                        <input id="IdTrabajo" name="IdTrabajo"  type="text" class="input" value="<?php echo $importation['IdTrabajo'];?>" readonly>
+                    </div>
+                    <div class="group">
+                        <label for="NombreTrabajo" class="label">Nombre Trabajo*:</label>
+                        <input id="NombreTrabajo" name="NombreTrabajo" type="text" class="input" value="<?php echo $importation['NombreTrabajo'];?>" readonly>
+                    </div>
+                    <div class="group">
+                        <label for="FechaIniTrabajo" class="label">Fecha Inicio*:</label>
+                        <input id="FechaIniTrabajo" type="date" name="FechaIniTrabajo" step="1" placeholder="Fecha Inicio"  value="<?php echo $importation['FechaIniTrabajo'];?>" readonly>
+                    </div>
+                    <div class="group">
+                        <label for="FechaFinTrabajo" class="label">Fecha Fin*:</label>
+                        <input id="FechaFinTrabajo" type="date" name="FechaFinTrabajo" step="1" placeholder="Fecha Fin"  value="<?php echo $importation['FechaFinTrabajo'];?>" readonly>
+                    </div>
+                    <div class="group">
+                        <label for="PorcentajeNota" class="label">Hora:</label>
+                        <input id="PorcentajeNota" name="PorcentajeNota" type="text" class="input" value="<?php echo $importation['PorcentajeNota'];?>">
+                    </div>
+                    <div class="group">
+                        <label for="Ruta" class="label">Ruta:</label>
+                        <input id="Ruta" name="Ruta" type="text" class="input" value="<?php echo $importation['Ruta'];?>">
+                    </div>
+
+                    <div class="group">
+                        <input id="send" type="submit" class="button" value="Editar">
+                    </div>
+                    <div class="backBttn">
+                        <a href="../Controllers/ImportationUser_SHOWALL_Controller.php"><i class="fa fa-arrow-circle-left" id="returnIcon"></i></a>
+                    </div>
+
+
+                </div>
+
+            </form>
+        </div>
+
+
+
+
+
+
+        <?php
+        include '../Locales/Footer.html';
+    }
 }
+?>
--- Views/ImportationUser_SHOWALL_View.php
+++ Views/ImportationUser_SHOWALL_View.php
@@ -8,5 +8,83 @@
 
 class ImportationUser_SHOWALL_View
 {
+    var $response;
+
+    function __construct($response){
+        $this->response = $response;
+        $this->render();
+    }
+
+    function render(){
+
+        /*include './Strings_SPANISH.php';*/
+        include '../Locales/Header.html';
+        include '../Locales/LateralBar.php';
+        ?>
+        <link rel="stylesheet" href="../Locales/Importation_SHOWALL.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <?php
+        echo '<h1 style="color: white;padding-left: 100px ">Se han encontrado '.'<span style="color: red;">' .mysqli_num_rows($this->response).'</span> Entrega(s).</h1>';
+        ?>
+
+
+        <div class="tableShowAll">
+
+            <div class="containTable">
+                <table class="table">
+                    <tr>
+                        <th>Id Trabajo</th>
+                        <th>Nombre Trabajo</th>
+                        <th>Fecha Inicio</th>
+                        <th>Fecha Fin</th>
+                        <th>Editar</th>
+                    </tr>
+
+
+
+                    <?php
+
+                    while ($work = mysqli_fetch_array($this->response)){
+                        if (strpos($work['IdTrabajo'], 'ET') !== false){
+                            echo "<tr>";
+                            echo"<td>".$work['IdTrabajo']."</td>".
+                                "<td>".$work['NombreTrabajo']."</td>".
+                                "<td>".$work['FechaIniTrabajo']."</td>".
+                                "<td>".$work['FechaFinTrabajo']."</td>".
+                                '<td><a href="../Controllers/ImportationUser_EDIT_Controller.php?value='.$work["IdTrabajo"].'"><i class="fa fa-pencil-square-o" id="modIcon"></i></td></a>'.
+                                "</tr>";
+                        }
+
+                    }
+
+
+
+
+
+                    ?>
+                </table>
+
+            </div>
+                <p class="mens">Accede a la entrega. Se te mostrará tu ficha de entrega con el Alias y podrás subir tu trabajo e indicar el número de horas utilizadas en la realización de la misma.</p>
+
+            <div class="ActionButtons">
+                <!-- <a href="../Controllers/User_SEARCH_Controller.php"><i class="fa fa-search" id="searchIcon"></i></a> -->
+                <a href="../Controllers/Group_ADD_Controller.php"><i class="fa fa-plus-square" id="addIcon" title="Añadir Grupo"></i></a>
+                <a href=' ../Controllers/Index_Controller.php'><i class="fa fa-arrow-circle-left " id="returnIcon" title="Volver"></i></a>
+            </div>
+
+        </div>
+
+
+
+
+
+
+        <?php
+        include '../Locales/Footer.html';
+    }
 
 }
+
+?>
